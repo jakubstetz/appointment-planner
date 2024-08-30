5897d9576883ec971e3ae34fe0afd840a812ff62
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-08-30 12:24:40 -0400

    Implement ContactForm component based on requirements.

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -8,6 +8,11 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <input id="name" value={name} type="text" placeholder="Enter name."></input>
+      <input id="phone" value={phone} type="text" placeholder="Enter phone number." pattern="^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$"></input>
+      <input id="emial" value={email} type="text" placeholder="Enter email."></input>
+      <input type="submit"></input>
+    </form>
   );
 };
