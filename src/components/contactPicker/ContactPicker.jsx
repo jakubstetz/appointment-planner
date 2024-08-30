29a93d15643ff304aed617e4f64a81c5f334dff6
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-08-30 14:20:31 -0400

    Implement ContactPicker component using given requirements.

--- src/components/contactPicker/ContactPicker.jsx
+++ src/components/contactPicker/ContactPicker.jsx
@@ -1,5 +1,8 @@
-export const ContactPicker = () => {
+export const ContactPicker = (contacts, changeHandler, value, name) => {
   return (
-    <></>
+    <select>
+      <option value="">No Contact Selected</option>
+      {contacts.map(({name}, _) => <option>{name}</option>)}
+    </select>
   );
 };
