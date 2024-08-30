c4f1b9a860b361d541ec3bfe39d68ed4b6044f8b
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-08-30 12:24:49 -0400

    Implement TileList component based on requirements.

--- src/components/tileList/TileList.jsx
+++ src/components/tileList/TileList.jsx
@@ -1,7 +1,7 @@
-export const TileList = () => {
+export const TileList = (array) => {
   return (
     <div>
-
+      {array.map((element, index) => <Tile name={element.name} description={element.email} key={index.toString()} />)}
     </div>
   );
 };
