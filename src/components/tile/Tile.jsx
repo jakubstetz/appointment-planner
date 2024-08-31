8b6331c0c8af0e4503df3a9f7dcdca2fe635dd8e
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-08-31 09:44:12 -0400

    Remove unnecessary brackets from Tile.jsx.

--- src/components/tile/Tile.jsx
+++ src/components/tile/Tile.jsx
@@ -1,7 +1,7 @@
 export const Tile = ({name, description}) => {
   return (
     <div className="tile-container">
-      <p name={'prop'} className="title-title">{name}</p>
+      <p name='prop' className="title-title">{name}</p>
       {Object.values(description).map((property, index) => (
         <p key={index} className="tile">{property}</p>
       ))}
